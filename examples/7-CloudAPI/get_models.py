31fefcf82510c136a15668744ebbe441466becca
scuervo <scuervo91@gmail.com>
2021-07-14 16:40:05 -0500

    add example

--- examples/7-CloudAPI/get_models.py
+++ examples/7-CloudAPI/get_models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import schema
 from dcapy.auth import Credential
 from dcapy.schedule import Period
 
@@ -7,3 +8,8 @@ cred.login('scuervo91','Fenicia1703')
 df = cred.get_models_info()
 
 print(df)
+
+
+df2 = cred.get_models_info(schema=['period','well'])
+
+print(df2)
