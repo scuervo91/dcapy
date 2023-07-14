43e8cf81984e04aa9e5ddd10fe45917611c3786d
Juan Pedro Bretti Mandarano <juanbretti@users.noreply.github.com>
2023-07-14 08:09:19 +0200

    Update arps.py
    Unified the definition of `di_factor`

--- dcapy/dca/arps.py
+++ dcapy/dca/arps.py
@@ -573,6 +573,7 @@ class Arps(BaseModel,DCA):
         #Check inputs
         x = df[time].values if isinstance(time,str) else time 
         yb = df[rate].values if isinstance(rate,str) else rate
+        di_factor = converter_factor('D', self.freq_di)
         #Expotential weighted average. If beta is 1 there's no effect
         y = exp_wgh_avg(yb,beta)
 
@@ -609,7 +610,6 @@ class Arps(BaseModel,DCA):
             #Optimization process
             popt, pcov = curve_fit(cost_function, x_filter, y_filter, bounds=([0.,0.,b_bounds[0]], [np.inf, np.inf, b_bounds[1]]))
             #Assign the results to the Class
-            di_factor = converter_factor('D', self.freq_di)
             self.qi = {'dist':'norm','kw':{'loc':popt[0],'scale':np.sqrt(np.diag(pcov)[0])}} if prob else popt[0] 
             self.di = {'dist':'norm','kw':{'loc':popt[1]*di_factor,'scale':np.sqrt(np.diag(pcov)[1])*di_factor}} if prob else popt[1]*di_factor
             self.b = {'dist':'norm','kw':{'loc':popt[2],'scale':np.sqrt(np.diag(pcov)[2])}} if prob else popt[2]
@@ -630,7 +630,6 @@ class Arps(BaseModel,DCA):
             #Optimization process
             popt, pcov = curve_fit(cost_function, x_filter, y_filter, bounds=(0.0, [np.inf, np.inf]))
             #Assign the results to the Class
-            di_factor = converter_factor('D', self.freq_di)
             self.qi = {'dist':'norm','kw':{'loc':popt[0],'scale':np.sqrt(np.diag(pcov)[0])}} if prob else popt[0] 
             self.di = {'dist':'norm','kw':{'loc':popt[1]*di_factor,'scale':np.sqrt(np.diag(pcov)[1])*di_factor}} if prob else popt[1]*di_factor
             self.ti = pd.Timestamp(x[total_filter==0][0]) if isinstance(x[total_filter==0][0],(np.datetime64,date)) else x[total_filter==0][0]
